ade4fa1394369cb46070d5940a9e0d590c7ef2c9
kuraomirako <wkwkhtrk@gmail.com>
2025-07-03 12:41:57 +0900

    カテゴリー一覧表示設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    @categories = Category.all
+    @genres = Genre.all
     @stories = Story.where(category_id: @category.id).order(created_at: :desc)
   end
 end
